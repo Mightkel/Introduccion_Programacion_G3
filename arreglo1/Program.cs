a072a6f20c628885854c98cb8119499c385fbc3b
Maykel <maykelcruz@uamv.edu.ni>
2026-05-06 10:06:07 -0600

    Ejercicios arreglos

---
+++ arreglo1/Program.cs
@@ -0,0 +1,38 @@
+﻿//almacenar 10 edades en un arreglo, luego mostrar el promedio de las edades, el maximo y el minimo
+//crea arreglo de edades
+int[] edades = new int[10];
+//pedir edades
+for (int i = 0; i < edades.Length; i++)
+{
+    try
+    {
+        
+    Console.WriteLine("Ingrese la edad " + (i + 1) + ":");
+    edades[i] = int.Parse(Console.ReadLine());
+
+    }
+    catch
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Error: Ingrese un número válido.");
+        i--;
+        Console.ResetColor();
+    }   
+}
+
+//calcular promedio, maximo y minimo
+int suma = 0, maximo = edades[0], minimo = edades[0];
+foreach (int edad in edades)
+{
+    suma += edad;
+    if (edad > maximo)
+        maximo = edad;
+    if (edad < minimo)
+        minimo = edad;
+}
+double promedio = (double)suma / edades.Length; 
+
+//mostrar resultados
+Console.WriteLine("El promedio de las edades es: " + promedio);
+Console.WriteLine("La edad máxima es: " + maximo);
+Console.WriteLine("La edad mínima es: " + minimo);
---
+++ arreglo2/Program.cs
@@ -0,0 +1,35 @@
+﻿//crea un arreglo de notas de 15 estudiantes, luego calcula el promedio de notas y los 3 primeros lugares.
+//no se puede ingresar notas menores a 0 ni mayores a 100
+int [] notas = new int[15];
+int suma = 0;
+double promedio = 0;
+
+for (int i = 0; i < notas.Length; i++)
+{
+    try
+    {
+        Console.WriteLine($"Ingrese la nota del estudiante {i + 1}: ");
+        int nota = int.Parse(Console.ReadLine());
+        if (nota < 0 || nota > 100)
+        {
+            throw new ArgumentOutOfRangeException("La nota debe ser entre 0 y 100.");
+        }
+        notas[i] = nota;
+        suma += nota;
+    }
+    catch (ArgumentOutOfRangeException ex)
+    {
+        Console.WriteLine(ex.Message);
+        i--; // Reintentar la misma posición
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Entrada inválida. Ingrese un número válido.");
+        i--; // Reintentar la misma posición
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ocurrió un error: {ex.Message}");
+        i--; // Reintentar la misma posición
+    }
+}
