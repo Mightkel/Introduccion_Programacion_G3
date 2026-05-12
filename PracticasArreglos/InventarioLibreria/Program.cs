1ac24ae75c43f50f46b8b0ad76b46bf3fc99930b
Maykel <maykelcruz@uamv.edu.ni>
2026-05-11 18:33:45 -0600

    Practica Arreglos

--- PracticasArreglos/InventarioLibreria/Program.cs
+++ PracticasArreglos/InventarioLibreria/Program.cs
@@ -1,2 +1,37 @@
 ﻿int [] libros = new int [5];
-string [] categorias = {"Romance", "Fantasía", "Novela", "Psicologo", "No ficcion"};
+string [] categorias = {"Romance", "Fantasía", "Novela", "Psicología", "No ficción"};
+string masventas = "";
+int mayor = 0, total = 0;
+double promedio;
+
+Console.Clear();
+Console.WriteLine("Inventario de Libreria");
+for (int i = 0; i < libros.Length; i++)
+{
+    try
+    {
+        Console.Write($"Ingrese la cantidad de libros vendidos de {categorias[i]}: ");
+        libros[i] = int.Parse(Console.ReadLine()!);
+        total += libros[i];
+    }
+    catch(FormatException)
+    {
+        Console.ForegroundColor=ConsoleColor.DarkRed;
+        Console.WriteLine("Valor invalido. Por favor, ingrese un numero entero");
+        Console.ResetColor();
+        i--;
+    }
+    if (libros[i] > mayor)
+    {
+        mayor = libros[i];
+        masventas = categorias[i];
+    }   
+}
+promedio = total/5;
+
+Console.ForegroundColor=ConsoleColor.DarkBlue;
+Console.WriteLine(" === Reporte del Inventario === ");
+Console.ResetColor();
+Console.WriteLine($"Total de libros vendidos: {total}");
+Console.WriteLine($"Categoria con más ventas: {masventas} ({mayor} libros vendidos)");
+Console.WriteLine($"Promedio de libros vendidos: {promedio}");
---
+++ PracticasArreglos/NotasEstudiantes/Program.cs
@@ -0,0 +1,56 @@
+﻿double [] notas = new double [8];
+int reprobados = 0;
+double totalnotas = 0, promedio;; 
+
+Console.Clear();
+Console.WriteLine("Ingrese las notas de ocho estudiantes:");
+for (int i = 0; i< notas.Length; i++)
+{
+    try
+    {
+        Console.Write($"Estudiante #{i+1}: ");
+        notas[i] = int.Parse(Console.ReadLine()!);
+        totalnotas += notas[i];
+        if (notas[i] < 0 || notas[i] > 100)
+        {
+            throw new ArgumentOutOfRangeException("La nota debe ser entre 0 y 100.");
+        }
+        if (notas[i] < 60)
+        {
+            reprobados++;
+        }
+    }
+    catch(ArgumentOutOfRangeException ex)
+    {
+        Console.ForegroundColor=ConsoleColor.DarkRed;
+        Console.WriteLine(ex.Message);
+        Console.ResetColor();
+        i--;
+    }
+    catch(FormatException)
+    {
+        Console.ForegroundColor=ConsoleColor.DarkRed;
+        Console.WriteLine("Valor invalido. Por favor, ingrese un numero");
+        Console.ResetColor();
+        i--;
+    }
+}
+promedio = totalnotas/8;
+
+Console.ForegroundColor=ConsoleColor.DarkBlue;
+Console.WriteLine(" === Informe de Calificaciones === ");
+Console.ResetColor();
+Console.WriteLine($"Promedio de Calificaciones: {promedio:N2}%");
+Console.WriteLine($"Cantidad de reprobados: {reprobados}");
+Console.WriteLine("Notas aprobadas:");
+for (int i = 0; i < notas.Length; i++)
+{
+    if (notas[i] < 60)
+    {
+        continue;
+    }
+    else
+    {
+        Console.WriteLine($"{notas[i]:N2}%");
+    }
+}
