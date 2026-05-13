b5fd48ed5544152d0a4ebe4cc0b5deee490c4d77
Maykel <maykelcruz@uamv.edu.ni>
2026-05-13 10:15:59 -0600

    Matriz

---
+++ matriz1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Matriz 3x3
+int[,] matriz = new int[3, 3];
+Console.Clear();
+Console.WriteLine("Digite los elementos de la matriz 3x3:");
+for (int f = 0; f < 3; f++)
+{
+    for (int c = 0; c < 3; c++)
+    {
+        Console.Write($"Elemento [{f}, {c}]: ");
+        matriz[f, c] = int.Parse(Console.ReadLine()!);
+    }
+}
+Console.WriteLine("Mostrar Matriz");
+for (int f = 0; f < 3; f++)
+{
+    for (int c = 0; c < 3; c++)
+    {
+        if (matriz[f,c] % 2 == 0)
+        {
+            Console.ForegroundColor=ConsoleColor.DarkGreen;
+            Console.Write($"{matriz[f,c]} ");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.Write($"{matriz[f, c]} ");
+            Console.ResetColor();
+        }
+    }
+    Console.WriteLine();
+}
